010bdf2a5c4cf518da5fb8d8d53917da84b70629
MisterX-byte-478104927 <eugendangutu@gmail.com>
2026-05-21 17:22:12 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-// DuoGPT Beta v2.1.1 by Mister X - STABLE REPAIR
+// DuoGPT Beta v2.2.1 by Mister X - STABLE REPAIR
 console.log("DuoGPT Beta v2.1.1 - Clean Core Running");
 
 let isWaiting = false;
