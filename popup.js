be88e217dcaeb240758a024c8094430e1465e0c9
MisterX-byte-478104927 <eugendangutu@gmail.com>
2026-05-22 21:49:16 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const statusDiv = document.getElementById('status');
   const versionCheckDiv = document.getElementById('version-check');
 
-  const currentVersion = "2.4.1"; // Versiunea curentă a extensiei
+  const currentVersion = "2.4.2"; // Versiunea curentă a extensiei
 
   // 1. Populăm limbile universale
   const languages = [
